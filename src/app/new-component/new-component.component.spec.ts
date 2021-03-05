8370b9a88a854b690f692c6378ceb0c6c8801985
Tanay Tiwari <tanay>
2021-03-05 11:09:55 +0530

    Anguar Features Modules implementation
    Anguar Features Modules implementation

---
+++ src/app/new-component/new-component.component.spec.ts
@@ -0,0 +1,26 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewComponentComponent } from './new-component.component';
+
+//describe defines a test suite
+describe('NewComponentComponent', () => {
+  let component: NewComponentComponent;
+  let fixture: ComponentFixture<NewComponentComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NewComponentComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
